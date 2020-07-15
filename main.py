711c86b66d5f4134bee8116edf0c70605f6656cc
Sol <ccw0711@naver.com>
2020-07-15 17:31:20 +0900

    update

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import os
+import sys
 import requests
 from bs4 import BeautifulSoup
+from links import *
 
 base = os.path.dirname(os.path.abspath(__file__)) + '/'
 
-firstUrl = 'http://www.yu.ac.kr/_korean/about/index.php?c=about_08_a_list'
 r = requests.get(firstUrl)
 soup = BeautifulSoup(r.text, 'html.parser')
 post = soup.findAll("tr")
-postNum = post[1].text[0:6] # txt파일의 postNum과 대치 후 다르면 새로 저장
-print(postNum)
+postNum = post[1].text[0:6]
+# print(postNum)
+
+with open(base + 'pointNum.txt', 'r') as chk:
+    preNum = chk.read().strip()
+    if preNum == postNum:
+        print('dd')
+        sys.exit()
+    else:
+        with open(base + 'pointNum.txt', 'w') as f:
+            f.write(postNum)
+            print('h')
+
+
 
 postTitle = soup.findAll("td", {'class' : 'title'})
 print(postTitle[0])
