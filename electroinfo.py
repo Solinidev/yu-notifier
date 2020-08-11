a1c0bad524ecacf8bc33a2924e5ef83972de1fee
Sol <ccw0711@naver.com>
2020-08-11 17:26:24 +0900

    update

--- electroinfo.py
+++ electroinfo.py
@@ -4,7 +4,18 @@ from links import *
 
 r = requests.get(electroinfoUrl)
 soup = BeautifulSoup(r.text, 'html.parser')
-post = soup.findAll("td", {'class' : 'B'})
-print(post[0].text)
+post = soup.findAll("tbody")
+# title = soup.findAll("td", {'class' : 'B'})
+
+title = soup.findAll("tr", {"style" : "height:30px;background:#fafafa;"})
+
+for i in title:
+    for j in i.findAll("td"):
+        # if j.text is None:
+        #     pass
+        if len(j.text) != 10:
+            pass
+        print(j.text)
+
 
 # 게시판 번호 없음, notice와 일반 게시글이 분리되어 있음
